b27aa5fdea7998ef61b51974d8ce8a74cddb952c
Darsein <albatross.darsein@gmail.com>
2017-07-05 01:39:43 +0900

    [SIF cospa] Enable macaron option and fix a bug

--- lovelive/sif/tools/event_cost_performance/event_points.js
+++ lovelive/sif/tools/event_cost_performance/event_points.js
@@ -1,21 +1,29 @@
 angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
-  .service("Event", function(Rank, ScoreMatch) {
+  .service("Event", function(Rank, ScoreMatch, Macaron) {
 
     var event = function() {
       this.rank = new Rank();
       this.reward = {};
       this.reward["score_match"] = new ScoreMatch();
+      this.reward["macaron"] = new Macaron();
 
-      this.event_type = "score_match";
-      this.difficulty = "expert";
-      this.score = "S";
+      this.event_type = 'macaron';
+      this.difficulty = 'expert';
+      this.task_difficulty = "expert";
+      this.score = 0;
+      this.combo = 0;
       this.ranking = 1;
       this.current_rank = 100;
       this.current_exp = 0;
       this.current_LP = 0;
       this.current_points = 0;
-      this.target_points = 50000;
-      this.average_points = Math.ceil(this.reward[this.event_type].base_points[this.difficulty] * this.reward[this.event_type].score_bonus[this.score] * this.reward[this.event_type].ranking_bonus[this.ranking]);
+      this.target_points = 25000;
+      this.macaron = 0;
+      if (this.event_type === "macaron") {
+        this.average_points = this.reward[this.event_type].get_points[this.task_difficulty][this.score][this.combo];
+      } else {
+        this.average_points = Math.ceil(this.reward[this.event_type].base_points[this.difficulty] * this.reward[this.event_type].score_bonus[this.score] * this.reward[this.event_type].ranking_bonus[this.ranking]);
+      }
     }
     var p = event.prototype;
 
@@ -35,10 +43,26 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
         var play_num = Math.floor(this.final_LP / LP_per_play);
         if (play_num === 0) break;
 
-        this.final_points += this.average_points * play_num;
+        if (this.event_type === 'macaron') {
+          this.final_points += this.reward[this.event_type].get_macarons[this.difficulty] * play_num;
+          this.final_macaron += this.reward[this.event_type].get_macarons[this.difficulty] * play_num;
+        } else {
+          this.final_points += this.average_points * play_num;
+        }
         this.final_LP -= LP_per_play * play_num;
         this.final_exp += exp_per_play * play_num;
         this.final_play_num += play_num;
+
+        if (this.event_type === 'macaron') {
+          var macaron_per_task_play = this.reward[this.event_type].required_macarons[this.task_difficulty];
+          var exp_per_task_play = this.reward[this.event_type].exp[this.task_difficulty];
+          var task_play_num = Math.floor(this.final_macaron / macaron_per_task_play);
+          this.final_macaron -= macaron_per_task_play * task_play_num;
+          this.final_exp += exp_per_task_play * task_play_num;
+          this.final_task_play_num += task_play_num;
+          this.final_points += this.average_points * task_play_num;
+        }
+
         while (this.next_exp <= this.final_exp) {
           this.final_exp -= this.next_exp;
           this.final_rank++;
@@ -55,13 +79,14 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
       this.max_LP = 25;
       this.max_LP += Math.floor(Math.min(this.current_rank, 300) / 2);
       this.max_LP += Math.floor(Math.max(this.current_rank - 300, 0) / 3);
-      console.log(this.current_rank, this.max_LP);
 
       this.final_rank = this.current_rank;
       this.final_exp = this.current_exp;
       this.final_LP = this.current_LP;
       this.final_points = this.current_points;
       this.final_play_num = 0;
+      this.final_task_play_num = 0;
+      this.final_macaron = this.macaron;
       this.used_stone = 0;
       this.next_exp = this.rank.rankTable[this.final_rank];
 
@@ -75,7 +100,8 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
         this.event_end = new Date(this.now.getFullYear(), this.now.getMonth() + 1, 0, 15);
       }
       [this.remain_day, this.remain_hour, this.remain_min] =
-          this.secToDateTuple( Math.floor((this.event_end - this.now)/1000) );
+          this.secToDateTuple( Math.floor((this.event_end - Math.max(this.now, this.event_start))/1000) );
+      this.final_LP += Math.floor((this.event_end - Math.max(this.now, this.event_start)) / 1000 / 60 / 6);
 
       this.consumeLP();
       while (this.final_points < this.target_points) {
@@ -85,7 +111,7 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
       }
 
       [this.required_day, this.required_hour, this.required_min] =
-          this.secToDateTuple(this.final_play_num * 3 * 60);
+          this.secToDateTuple((this.final_play_num + this.final_task_play_num) * 3 * 60);
     }
 
     return event;
@@ -94,12 +120,19 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
     $scope.event = new Event();
 
     $scope.$watchGroup([
+      'event.event_type',
+      'event.task_difficulty',
       'event.difficulty',
       'event.score',
+      'event.combo',
       'event.ranking',
     ], function(newVal, oldVal) {
       // Score: S, Ranking: 2nd
-      $scope.event.average_points = Math.ceil($scope.event.reward[$scope.event.event_type].base_points[$scope.event.difficulty] * $scope.event.reward[$scope.event.event_type].score_bonus[$scope.event.score] * $scope.event.reward[$scope.event.event_type].ranking_bonus[$scope.event.ranking]);
+      if ($scope.event.event_type == "macaron") {
+        $scope.event.average_points = $scope.event.reward[$scope.event.event_type].get_points[$scope.event.task_difficulty][$scope.event.score][$scope.event.combo];
+      } else {
+        $scope.event.average_points = Math.ceil($scope.event.reward[$scope.event.event_type].base_points[$scope.event.difficulty] * $scope.event.reward[$scope.event.event_type].score_bonus[$scope.event.score] * $scope.event.reward[$scope.event.event_type].ranking_bonus[$scope.event.ranking]);
+      }
       $scope.event.calc();
     });
 
@@ -112,5 +145,10 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
       'event.target_points',
     ], function(newVal, oldVal) {
       $scope.event.calc();
+    });
+
+    $scope.$watch('event.event_type', function(newVal, oldVal) {
+      if ($scope.event.event_type === 'score_match') $scope.event.target_points = 50000;
+      if ($scope.event.event_type === 'macaron') $scope.event.target_points = 25000;
     });
   });
--- lovelive/sif/tools/event_cost_performance/points.js
+++ lovelive/sif/tools/event_cost_performance/points.js
@@ -7,28 +7,25 @@ angular.module('points', [])
       this.required_LP["normal"] = 10;
       this.required_LP["hard"] = 15;
       this.required_LP["expert"] = 25;
-      this.required_LP["technical"] = 25;
 
       this.exp = {};
       this.exp["easy"] = 12;
       this.exp["normal"] = 26;
       this.exp["hard"] = 46;
       this.exp["expert"] = 83;
-      this.exp["technical"] = 83;
 
       this.base_points = {};
       this.base_points["easy"] = 42;
       this.base_points["normal"] = 100;
       this.base_points["hard"] = 177;
       this.base_points["expert"] = 357;
-      this.base_points["technical"] = 357;
 
       this.score_bonus = {};
-      this.score_bonus["S"] = 1.20;
-      this.score_bonus["A"] = 1.15;
-      this.score_bonus["B"] = 1.10;
-      this.score_bonus["C"] = 1.05;
-      this.score_bonus["D"] = 1.00;
+      this.score_bonus[0] = 1.20;
+      this.score_bonus[1] = 1.15;
+      this.score_bonus[2] = 1.10;
+      this.score_bonus[3] = 1.05;
+      this.score_bonus[4] = 1.00;
 
       this.ranking_bonus = {};
       this.ranking_bonus[1] = 1.25;
@@ -37,4 +34,43 @@ angular.module('points', [])
       this.ranking_bonus[4] = 1.00;
     }
     return scoreMatch;
+  })
+  .service("Macaron",
+  function() {
+    var macaron = function() {
+      this.required_LP = {};
+      this.required_LP["easy"] = 5;
+      this.required_LP["normal"] = 10;
+      this.required_LP["hard"] = 15;
+      this.required_LP["expert"] = 25;
+
+      this.required_macarons = {};
+      this.required_macarons["easy"] = 15;
+      this.required_macarons["normal"] = 30;
+      this.required_macarons["hard"] = 45;
+      this.required_macarons["expert"] = 75;
+
+      this.exp = {};
+      this.exp["easy"] = 12;
+      this.exp["normal"] = 26;
+      this.exp["hard"] = 46;
+      this.exp["expert"] = 83;
+
+      this.get_macarons = {};
+      this.get_macarons["easy"] = 5;
+      this.get_macarons["normal"] = 10;
+      this.get_macarons["hard"] = 16;
+      this.get_macarons["expert"] = 27;
+
+      this.get_points = {};
+      this.get_points["hard"] = [ [261, 254, 246, 241, 237],
+                                  [249, 242, 235, 230, 226],
+                                  [237, 231, 224, 220, 215],
+                                  [226, 219, 213, 209, 204] ];
+      this.get_points["expert"] = [ [565, 549, 518, 508, 498],
+                                     [540, 525, 495, 485, 475],
+                                     [509, 495, 467, 458, 448],
+                                     [484, 470, 444, 435, 426] ];
+    }
+    return macaron;
   });
