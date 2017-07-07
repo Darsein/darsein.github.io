0b99ded428f2162801a044d50e84950c91d2c493
Darsein <albatross.darsein@gmail.com>
2017-07-08 00:03:57 +0900

    [SIF cospa] add rewards

--- lovelive/sif/tools/event_cost_performance/event_points.js
+++ lovelive/sif/tools/event_cost_performance/event_points.js
@@ -3,11 +3,11 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
 
     var event = function() {
       this.rank = new Rank();
-      this.reward = {};
-      this.reward["score_match"] = new ScoreMatch();
-      this.reward["macaron"] = new Macaron();
+      this.event_type = {};
+      this.event_type["score_match"] = new ScoreMatch();
+      this.event_type["macaron"] = new Macaron();
 
-      this.event_type = 'macaron';
+      this.event_name = 'macaron';
       this.difficulty = 'expert';
       this.task_difficulty = "expert";
       this.score = 0;
@@ -19,10 +19,10 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
       this.current_points = 0;
       this.target_points = 25000;
       this.macaron = 0;
-      if (this.event_type === "macaron") {
-        this.average_points = this.reward[this.event_type].get_points[this.task_difficulty][this.score][this.combo];
+      if (this.event_name === "macaron") {
+        this.average_points = this.event_type[this.event_name].get_points[this.task_difficulty][this.score][this.combo];
       } else {
-        this.average_points = Math.ceil(this.reward[this.event_type].base_points[this.difficulty] * this.reward[this.event_type].score_bonus[this.score] * this.reward[this.event_type].ranking_bonus[this.ranking]);
+        this.average_points = Math.ceil(this.event_type[this.event_name].base_points[this.difficulty] * this.event_type[this.event_name].score_bonus[this.score] * this.event_type[this.event_name].ranking_bonus[this.ranking]);
       }
     }
     var p = event.prototype;
@@ -37,15 +37,15 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
     }
 
     p.consumeLP = function() {
-      var LP_per_play = this.reward[this.event_type].required_LP[this.difficulty];
-      var exp_per_play = this.reward[this.event_type].exp[this.difficulty];
+      var LP_per_play = this.event_type[this.event_name].required_LP[this.difficulty];
+      var exp_per_play = this.event_type[this.event_name].exp[this.difficulty];
       while (true) {
         var play_num = Math.floor(this.final_LP / LP_per_play);
         if (play_num === 0) break;
 
-        if (this.event_type === 'macaron') {
-          this.final_points += this.reward[this.event_type].get_macarons[this.difficulty] * play_num;
-          this.final_macaron += this.reward[this.event_type].get_macarons[this.difficulty] * play_num;
+        if (this.event_name === 'macaron') {
+          this.final_points += this.event_type[this.event_name].get_macarons[this.difficulty] * play_num;
+          this.final_macaron += this.event_type[this.event_name].get_macarons[this.difficulty] * play_num;
         } else {
           this.final_points += this.average_points * play_num;
         }
@@ -53,9 +53,9 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
         this.final_exp += exp_per_play * play_num;
         this.final_play_num += play_num;
 
-        if (this.event_type === 'macaron') {
-          var macaron_per_task_play = this.reward[this.event_type].required_macarons[this.task_difficulty];
-          var exp_per_task_play = this.reward[this.event_type].exp[this.task_difficulty];
+        if (this.event_name === 'macaron') {
+          var macaron_per_task_play = this.event_type[this.event_name].required_macarons[this.task_difficulty];
+          var exp_per_task_play = this.event_type[this.event_name].exp[this.task_difficulty];
           var task_play_num = Math.floor(this.final_macaron / macaron_per_task_play);
           this.final_macaron -= macaron_per_task_play * task_play_num;
           this.final_exp += exp_per_task_play * task_play_num;
@@ -102,7 +102,6 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
       [this.remain_day, this.remain_hour, this.remain_min] =
           this.secToDateTuple( Math.floor((this.event_end - Math.max(this.now, this.event_start))/1000) );
       this.final_LP += Math.floor((this.event_end - Math.max(this.now, this.event_start)) / 1000 / 60 / 6);
-      console.log(this.final_LP);
 
       this.consumeLP();
       while (this.final_points < this.target_points) {
@@ -113,6 +112,14 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
 
       [this.required_day, this.required_hour, this.required_min] =
           this.secToDateTuple((this.final_play_num + this.final_task_play_num) * 3 * 60);
+
+      this.rewards = {};
+      for (var reward of this.event_type[this.event_name].event_rewards) {
+        if (reward["required_points"] <= this.final_points) {
+          if (!this.rewards[reward["category"]]) this.rewards[reward["category"]] = 0;
+          this.rewards[reward["category"]] += reward["number"];
+        }
+      }
     }
 
     return event;
@@ -121,7 +128,7 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
     $scope.event = new Event();
 
     $scope.$watchGroup([
-      'event.event_type',
+      'event.event_name',
       'event.task_difficulty',
       'event.difficulty',
       'event.score',
@@ -129,10 +136,10 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
       'event.ranking',
     ], function(newVal, oldVal) {
       // Score: S, Ranking: 2nd
-      if ($scope.event.event_type == "macaron") {
-        $scope.event.average_points = $scope.event.reward[$scope.event.event_type].get_points[$scope.event.task_difficulty][$scope.event.score][$scope.event.combo];
+      if ($scope.event.event_name == "macaron") {
+        $scope.event.average_points = $scope.event.event_type[$scope.event.event_name].get_points[$scope.event.task_difficulty][$scope.event.score][$scope.event.combo];
       } else {
-        $scope.event.average_points = Math.ceil($scope.event.reward[$scope.event.event_type].base_points[$scope.event.difficulty] * $scope.event.reward[$scope.event.event_type].score_bonus[$scope.event.score] * $scope.event.reward[$scope.event.event_type].ranking_bonus[$scope.event.ranking]);
+        $scope.event.average_points = Math.ceil($scope.event.event_type[$scope.event.event_name].base_points[$scope.event.difficulty] * $scope.event.event_type[$scope.event.event_name].score_bonus[$scope.event.score] * $scope.event.event_type[$scope.event.event_name].ranking_bonus[$scope.event.ranking]);
       }
       $scope.event.calc();
     });
@@ -149,8 +156,8 @@ angular.module('darsein-hp', ['ngMaterial', 'rank', 'points'])
       $scope.event.calc();
     });
 
-    $scope.$watch('event.event_type', function(newVal, oldVal) {
-      if ($scope.event.event_type === 'score_match') $scope.event.target_points = 50000;
-      if ($scope.event.event_type === 'macaron') $scope.event.target_points = 25000;
+    $scope.$watch('event.event_name', function(newVal, oldVal) {
+      if ($scope.event.event_name === 'score_match') $scope.event.target_points = 50000;
+      if ($scope.event.event_name === 'macaron') $scope.event.target_points = 25000;
     });
   });
--- lovelive/sif/tools/event_cost_performance/points.js
+++ lovelive/sif/tools/event_cost_performance/points.js
@@ -1,76 +1,201 @@
 angular.module('points', [])
   .service("ScoreMatch",
-  function() {
-    var scoreMatch = function() {
-      this.required_LP = {};
-      this.required_LP["easy"] = 5;
-      this.required_LP["normal"] = 10;
-      this.required_LP["hard"] = 15;
-      this.required_LP["expert"] = 25;
+    function() {
+      var scoreMatch = function() {
+        this.required_LP = {};
+        this.required_LP["easy"] = 5;
+        this.required_LP["normal"] = 10;
+        this.required_LP["hard"] = 15;
+        this.required_LP["expert"] = 25;
 
-      this.exp = {};
-      this.exp["easy"] = 12;
-      this.exp["normal"] = 26;
-      this.exp["hard"] = 46;
-      this.exp["expert"] = 83;
+        this.exp = {};
+        this.exp["easy"] = 12;
+        this.exp["normal"] = 26;
+        this.exp["hard"] = 46;
+        this.exp["expert"] = 83;
 
-      this.base_points = {};
-      this.base_points["easy"] = 42;
-      this.base_points["normal"] = 100;
-      this.base_points["hard"] = 177;
-      this.base_points["expert"] = 357;
+        this.base_points = {};
+        this.base_points["easy"] = 42;
+        this.base_points["normal"] = 100;
+        this.base_points["hard"] = 177;
+        this.base_points["expert"] = 357;
 
-      this.score_bonus = {};
-      this.score_bonus[0] = 1.20;
-      this.score_bonus[1] = 1.15;
-      this.score_bonus[2] = 1.10;
-      this.score_bonus[3] = 1.05;
-      this.score_bonus[4] = 1.00;
+        this.score_bonus = {};
+        this.score_bonus[0] = 1.20;
+        this.score_bonus[1] = 1.15;
+        this.score_bonus[2] = 1.10;
+        this.score_bonus[3] = 1.05;
+        this.score_bonus[4] = 1.00;
 
-      this.ranking_bonus = {};
-      this.ranking_bonus[1] = 1.25;
-      this.ranking_bonus[2] = 1.15;
-      this.ranking_bonus[3] = 1.05;
-      this.ranking_bonus[4] = 1.00;
-    }
-    return scoreMatch;
-  })
+        this.ranking_bonus = {};
+        this.ranking_bonus[1] = 1.25;
+        this.ranking_bonus[2] = 1.15;
+        this.ranking_bonus[3] = 1.05;
+        this.ranking_bonus[4] = 1.00;
+
+        // (required points, categories, amount)
+        this.event_rewards = [
+          {required_points: 20, category: "G", number: 5000},
+          {required_points: 50, category: "友情pt", number: 50},
+          {required_points: 100, category: "G", number: 5500},
+          {required_points: 250, category: "友情pt", number: 100},
+          {required_points: 500, category: "ラブカストーン", number: 1},
+          {required_points: 750, category: "G", number: 6000},
+          {required_points: 1000, category: "友情pt", number: 150},
+          {required_points: 1200, category: "G", number: 6500},
+          {required_points: 1500, category: "友情pt", number: 200},
+          {required_points: 2000, category: "G", number: 7000},
+          {required_points: 2500, category: "ラブカストーン", number: 1},
+          {required_points: 3000, category: "友情pt", number: 250},
+          {required_points: 3500, category: "G", number: 7500},
+          {required_points: 4000, category: "友情pt", number: 300},
+          {required_points: 4500, category: "N部員", number: 1},
+          {required_points: 5000, category: "ラブカストーン", number: 1},
+          {required_points: 5750, category: "友情pt", number: 350},
+          {required_points: 6500, category: "G", number: 10000},
+          {required_points: 7250, category: "友情pt", number: 400},
+          {required_points: 8000, category: "G", number: 12500},
+          {required_points: 8750, category: "友情pt", number: 450},
+          {required_points: 9500, category: "G", number: 15000},
+          {required_points: 11000, category: "ラブカストーン", number: 1},
+          {required_points: 12500, category: "Rアニマル", number: 1},
+          {required_points: 15000, category: "友情pt", number: 500},
+          {required_points: 17500, category: "R先生", number: 1},
+          {required_points: 20000, category: "ラブカストーン", number: 1},
+          {required_points: 22500, category: "友情pt", number: 550},
+          {required_points: 25000, category: "SR部員", number: 1},
+          {required_points: 27500, category: "友情pt", number: 600},
+          {required_points: 30000, category: "G", number: 20000},
+          {required_points: 32500, category: "ラブカストーン", number: 2},
+          {required_points: 35000, category: "友情pt", number: 700},
+          {required_points: 37500, category: "G", number: 25000},
+          {required_points: 40000, category: "友情pt", number: 800},
+          {required_points: 42500, category: "R先生", number: 1},
+          {required_points: 45000, category: "G", number: 30000},
+          {required_points: 47500, category: "ラブカストーン", number: 2},
+          {required_points: 50000, category: "友情pt", number: 900},
+          {required_points: 52500, category: "G", number: 40000},
+          {required_points: 55000, category: "友情pt", number: 1000},
+          {required_points: 57500, category: "G", number: 50000},
+          {required_points: 60000, category: "SR部員", number: 1},
+          {required_points: 62500, category: "ラブカストーン", number: 2},
+          {required_points: 65000, category: "友情pt", number: 1100},
+          {required_points: 67500, category: "G", number: 75000},
+          {required_points: 70000, category: "友情pt", number: 1200},
+          {required_points: 75000, category: "勧誘チケット", number: 1},
+          {required_points: 80000, category: "ラブカストーン", number: 2},
+          {required_points: 90000, category: "友情pt", number: 1300},
+          {required_points: 100000, category: "SR部員", number: 1},
+          {required_points: 110000, category: "ラブカストーン", number: 3},
+          {required_points: 120000, category: "友情pt", number: 1400},
+          {required_points: 130000, category: "勧誘チケット", number: 1},
+          {required_points: 140000, category: "友情pt", number: 1500},
+          {required_points: 150000, category: "G", number: 100000},
+          {required_points: 160000, category: "ラブカストーン", number: 4},
+        ]
+      }
+      return scoreMatch;
+    })
   .service("Macaron",
-  function() {
-    var macaron = function() {
-      this.required_LP = {};
-      this.required_LP["easy"] = 5;
-      this.required_LP["normal"] = 10;
-      this.required_LP["hard"] = 15;
-      this.required_LP["expert"] = 25;
+    function() {
+      var macaron = function() {
+        this.required_LP = {};
+        this.required_LP["easy"] = 5;
+        this.required_LP["normal"] = 10;
+        this.required_LP["hard"] = 15;
+        this.required_LP["expert"] = 25;
 
-      this.required_macarons = {};
-      this.required_macarons["easy"] = 15;
-      this.required_macarons["normal"] = 30;
-      this.required_macarons["hard"] = 45;
-      this.required_macarons["expert"] = 75;
+        this.required_macarons = {};
+        this.required_macarons["easy"] = 15;
+        this.required_macarons["normal"] = 30;
+        this.required_macarons["hard"] = 45;
+        this.required_macarons["expert"] = 75;
 
-      this.exp = {};
-      this.exp["easy"] = 12;
-      this.exp["normal"] = 26;
-      this.exp["hard"] = 46;
-      this.exp["expert"] = 83;
+        this.exp = {};
+        this.exp["easy"] = 12;
+        this.exp["normal"] = 26;
+        this.exp["hard"] = 46;
+        this.exp["expert"] = 83;
 
-      this.get_macarons = {};
-      this.get_macarons["easy"] = 5;
-      this.get_macarons["normal"] = 10;
-      this.get_macarons["hard"] = 16;
-      this.get_macarons["expert"] = 27;
+        this.get_macarons = {};
+        this.get_macarons["easy"] = 5;
+        this.get_macarons["normal"] = 10;
+        this.get_macarons["hard"] = 16;
+        this.get_macarons["expert"] = 27;
 
-      this.get_points = {};
-      this.get_points["hard"] = [ [261, 254, 246, 241, 237],
-                                  [249, 242, 235, 230, 226],
-                                  [237, 231, 224, 220, 215],
-                                  [226, 219, 213, 209, 204] ];
-      this.get_points["expert"] = [ [565, 549, 518, 508, 498],
-                                     [540, 525, 495, 485, 475],
-                                     [509, 495, 467, 458, 448],
-                                     [484, 470, 444, 435, 426] ];
-    }
-    return macaron;
-  });
+        this.get_points = {};
+        this.get_points["hard"] = [
+          [261, 254, 246, 241, 237],
+          [249, 242, 235, 230, 226],
+          [237, 231, 224, 220, 215],
+          [226, 219, 213, 209, 204]
+        ];
+        this.get_points["expert"] = [
+          [565, 549, 518, 508, 498],
+          [540, 525, 495, 485, 475],
+          [509, 495, 467, 458, 448],
+          [484, 470, 444, 435, 426]
+        ];
+
+        // (required points, categories, amount)
+        this.event_rewards = [
+          {required_points: 10, category: "G", number: 5000},
+          {required_points: 50, category: "友情pt", number: 50},
+          {required_points: 100, category: "G", number: 5500},
+          {required_points: 150, category: "友情pt", number: 100},
+          {required_points: 200, category: "ラブカストーン", number: 1},
+          {required_points: 300, category: "G", number: 6000},
+          {required_points: 450, category: "友情pt", number: 150},
+          {required_points: 600, category: "G", number: 6500},
+          {required_points: 850, category: "友情pt", number: 200},
+          {required_points: 1000, category: "G", number: 7000},
+          {required_points: 1200, category: "ラブカストーン", number: 1},
+          {required_points: 1400, category: "友情pt", number: 250},
+          {required_points: 1600, category: "G", number: 7500},
+          {required_points: 1800, category: "友情pt", number: 300},
+          {required_points: 2000, category: "N部員", number: 1},
+          {required_points: 2250, category: "ラブカストーン", number: 1},
+          {required_points: 2500, category: "友情pt", number: 350},
+          {required_points: 2750, category: "G", number: 10000},
+          {required_points: 3000, category: "友情pt", number: 400},
+          {required_points: 3500, category: "G", number: 12500},
+          {required_points: 4000, category: "友情pt", number: 450},
+          {required_points: 4500, category: "G", number: 15000},
+          {required_points: 5000, category: "ラブカストーン", number: 1},
+          {required_points: 6000, category: "Rアニマル", number: 1},
+          {required_points: 7000, category: "友情pt", number: 500},
+          {required_points: 8000, category: "R先生", number: 1},
+          {required_points: 9000, category: "ラブカストーン", number: 1},
+          {required_points: 10000, category: "友情pt", number: 550},
+          {required_points: 11000, category: "SR部員", number: 1},
+          {required_points: 12000, category: "友情pt", number: 600},
+          {required_points: 13000, category: "G", number: 20000},
+          {required_points: 14000, category: "ラブカストーン", number: 2},
+          {required_points: 15000, category: "友情pt", number: 700},
+          {required_points: 16000, category: "G", number: 25000},
+          {required_points: 17000, category: "友情pt", number: 800},
+          {required_points: 18000, category: "R先生", number: 1},
+          {required_points: 19000, category: "G", number: 30000},
+          {required_points: 20000, category: "ラブカストーン", number: 2},
+          {required_points: 21000, category: "友情pt", number: 900},
+          {required_points: 22000, category: "G", number: 40000},
+          {required_points: 23000, category: "友情pt", number: 1000},
+          {required_points: 24000, category: "G", number: 50000},
+          {required_points: 25000, category: "SR部員", number: 1},
+          {required_points: 26500, category: "ラブカストーン", number: 2},
+          {required_points: 28000, category: "友情pt", number: 1100},
+          {required_points: 30000, category: "G", number: 75000},
+          {required_points: 32000, category: "友情pt", number: 1200},
+          {required_points: 34000, category: "勧誘チケット", number: 1},
+          {required_points: 36000, category: "ラブカストーン", number: 2},
+          {required_points: 38000, category: "友情pt", number: 1300},
+          {required_points: 40000, category: "SR部員", number: 1},
+          {required_points: 42500, category: "ラブカストーン", number: 3},
+          {required_points: 45000, category: "友情pt", number: 1400},
+          {required_points: 50000, category: "勧誘チケット", number: 1},
+          {required_points: 55000, category: "友情pt", number: 1500},
+          {required_points: 60000, category: "G", number: 100000},
+          {required_points: 65000, category: "ラブカストーン", number: 4},        ]
+      }
+      return macaron;
+    });
