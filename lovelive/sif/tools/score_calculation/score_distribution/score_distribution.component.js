207f4e7c7d6f3585075664b859f0750ab2ac8bf4
Darsein <albatross.darsein@gmail.com>
2017-08-28 08:33:48 +0900

    [SIF score] added placeholder of score distribution view.

---
+++ lovelive/sif/tools/score_calculation/score_distribution/score_distribution.component.js
@@ -0,0 +1,7 @@
+angular.module('unitScore')
+  .component('scoreDistribution', {
+    templateUrl: 'score_distribution/score_distribution.template.html',
+    controller: function scoreDistributionController($scope, $rootScope) {
+
+    }
+  });
