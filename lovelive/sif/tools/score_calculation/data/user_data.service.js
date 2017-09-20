d09ae48e22c448a57f2dec5080eeaa500cf37a27
Darsein <albatross.darsein@gmail.com>
2017-09-20 23:38:46 +0900

    [SIF score] minor change

--- lovelive/sif/tools/score_calculation/data/user_data.service.js
+++ lovelive/sif/tools/score_calculation/data/user_data.service.js
@@ -13,9 +13,17 @@ angular.module('data')
       // initialize user's card list
       this.own_card_list = [];
 
+      // temporal
+      for (var i=0; i<9; ++i) {
+        this.own_card_list.push(
+         { id: 94, kizuna: 500, level: 80, slot: 4, skill_level: 1, SIS: [],}
+        );
+      }
       this.unit_members = [];
       for (var i=0; i<9; ++i) {
-        this.unit_members.push(-1);
+        // this.unit_members.push(-1);
+        // temporal
+        this.unit_members.push(i);
       }
     };
     return userData;
