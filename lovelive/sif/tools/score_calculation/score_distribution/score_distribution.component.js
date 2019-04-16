1a354d70c88993a9c0c3f7dd6ebc978aa59e1c63
Darsein <albatross.darsein@gmail.com>
2019-04-17 01:27:47 +0900

    [SIF score] allow skill level greater than 8

--- lovelive/sif/tools/score_calculation/score_distribution/score_distribution.component.js
+++ lovelive/sif/tools/score_calculation/score_distribution/score_distribution.component.js
@@ -438,8 +438,9 @@ angular.module('unitScore')
                   var skill = {};
                   skill.type = card.skill.type;
                   skill.prob = card.skill.stats_list[card.skill_level - 1][0];
-                  skill.value = card.skill.stats_list[Math.min(8, card.skill_level + skill_boost) - 1][1];
-                  skill.term = card.skill.stats_list[Math.min(8, card.skill_level + skill_boost) - 1][2];
+                  var max_skill_level = card.skill.stats_list.length;
+                  skill.value = card.skill.stats_list[Math.min(max_skill_level, card.skill_level + skill_boost) - 1][1];
+                  skill.term = card.skill.stats_list[Math.min(max_skill_level, card.skill_level + skill_boost) - 1][2];
                   skill.required = card.skill.stats_list[card.skill_level - 1][3];
 
                   var prob = skill.prob * prob_bonus;
