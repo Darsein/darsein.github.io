41ce7275719c992ce4b5f7e615e0ffdce7320c03
Darsein <albatross.darsein@gmail.com>
2020-05-04 18:47:04 +0900

    add edit mode

--- lovelive/member_place/member_place/member_place.component.js
+++ lovelive/member_place/member_place/member_place.component.js
@@ -5,13 +5,11 @@ angular.module('darsein-hp')
       var self = this;
 
       self.getStoredSelectedPuzzleId = function () {
-        console.log($cookies.get('selected_puzzle_id'));
         return $cookies.get('selected_puzzle_id')
           ? JSON.parse($cookies.get('selected_puzzle_id')) : 0;
       };
 
       self.getStoredBoard = function (id) {
-        console.log('board' + id, $cookies.get('board' + id));
         return $cookies.get('board' + id)
           ? JSON.parse($cookies.get('board' + id))
           : Array.from(new Array(9), () => new Array(9).fill(0));
@@ -21,6 +19,7 @@ angular.module('darsein-hp')
 
       self.selected_puzzle = self.board_data.puzzles[self.getStoredSelectedPuzzleId()];
       self.current_board = self.getStoredBoard(self.selected_puzzle.id);
+      self.overwrite_character = -1;
 
       self.changeSelectedPuzzle = function() {
         self.selected_group = self.board_data.puzzles[self.selected_puzzle.id].group;
@@ -33,17 +32,29 @@ angular.module('darsein-hp')
       };
 
       self.getCellValue = function(row, col) {
-        return self.selected_board[row][col] != 0
+        return self.selected_board[row][col] !== 0
                    ? self.selected_board[row][col]
                    : self.current_board[row][col];
       };
 
-      self.incrementCell = function(row, col) {
-        if (self.selected_board[row][col] != 0) {
+      self.isDefaultCell = function(row, col) {
+        return self.selected_board[row][col] !== 0;
+      }
+
+      self.changeCellValue = function(row, col) {
+        if (self.isDefaultCell(row, col)) {
           return;
         }
-        self.current_board[row][col]++;
-        self.current_board[row][col] %= 10;
+        if (self.overwrite_character === -1) {
+          self.current_board[row][col]++;
+          self.current_board[row][col] %= 10;
+        } else {
+          self.current_board[row][col] = self.overwrite_character;
+        }
+      };
+
+      self.changeOverwriteCharactor = function(id) {
+        self.overwrite_character = id;
       };
 
       self.resetState = function() {
@@ -57,7 +68,6 @@ angular.module('darsein-hp')
       $scope.$watch(function() {
         return self.current_board;
       }, function(newVal, oldVal) {
-        console.log(self.current_board);
         var expire = new Date();
         expire.setMonth(expire.getMonth() + 3);
         $cookies.put('board' + self.selected_puzzle.id,
